8e8e40f0a2cbc2168e1633ef8bc18ec8ea4a197d
pvermees <pieter.vermeesch@gmail.com>
2017-07-17 02:07:31 +0100

    Consistency between two modes of Ludwig regression

--- R/ludwig.R
+++ R/ludwig.R
@@ -35,7 +35,12 @@ ludwig.UPb <- function(x,exterr=FALSE,...){
     out$cov <- tryCatch({ # analytical
         fish <- fisher.lud(x,ta0b0=fit$par,exterr=exterr)
         ns <- length(x)
-        solve(fish[(ns+1):(ns+3),(ns+1):(ns+3)])
+        # block inversion:
+        AA <- fish[1:ns,1:ns]
+        BB <- fish[1:ns,(ns+1):(ns+3)]
+        CC <- fish[(ns+1):(ns+3),1:ns]
+        DD <- fish[(ns+1):(ns+3),(ns+1):(ns+3)]
+        solve(DD - CC %*% solve(AA) %*% BB)
     }, error = function(e){ # numerical
         fit <- optim(init,fn=LL.lud.UPb,method="BFGS",x=x,exterr=exterr,hessian=TRUE)
         solve(fit$hessian)
@@ -124,42 +129,7 @@ fisher_lud_with_decay_err <- function(tt,a0,b0,z,omega){
     d2L.da0dt <- 0
     d2L.db0dt <- 0
     d2L.da0db0 <- 0
-    for (i in 1:ns){
-        i1 <- i
-        i2 <- i + ns
-        i3 <- i + 2*ns
-        for (j in 1:ns){
-            j1 <- j
-            j2 <- j + ns
-            j3 <- j + 2*ns
-            d2L.dt2 <- d2L.dt2 +
-                omega[i1,j1]*Q235^2 +
-                omega[i2,j2]*Q238^2 +
-                2*Q235*Q238*omega[i1,j2]
-            d2L.da02 <- d2L.da02 +
-                z[i]*z[j]*omega[i2,j2]
-            d2L.db02 <- d2L.db02 +
-                z[i]*z[j]*omega[i1,j1]*U^2
-            d2L.da0dt <- d2L.da0dt +
-                Q238*(z[i]+z[j])*omega[i2,j2]/2 +
-                Q235*z[j]*omega[i1,j1]
-            d2L.db0dt <- d2L.db0dt + U*(
-                Q235*(z[i]+z[j])*omega[i1,j1]/2 +
-                Q238*z[i]*omega[i1,j2] )
-            d2L.da0db0 <- d2L.da0db0 +
-                U * z[i]*z[j]*omega[i1,j2]
-        }
-    }
     out <- matrix(0,ns+3,ns+3)
-    out[ns+1,ns+1] <- d2L.dt2 # m11
-    out[ns+2,ns+2] <- d2L.da02 # m22
-    out[ns+3,ns+3] <- d2L.db02 # m33
-    out[ns+1,ns+2] <- d2L.da0dt # m12
-    out[ns+1,ns+3] <- d2L.db0dt # m13
-    out[ns+2,ns+1] <- d2L.da0dt # m12
-    out[ns+3,ns+1] <- d2L.db0dt # m13
-    out[ns+2,ns+3] <- d2L.da0db0 # m23
-    out[ns+3,ns+2] <- d2L.da0db0 # m23
     for (i in 1:ns){
         i1 <- i
         i2 <- i + ns
@@ -172,29 +142,50 @@ fisher_lud_with_decay_err <- function(tt,a0,b0,z,omega){
             j1 <- j
             j2 <- j + ns
             j3 <- j + 2*ns
+            d2L.dt2 <- d2L.dt2 + omega[i1,j1]*Q235^2 +
+                omega[i2,j2]*Q238^2 + 2*Q235*Q238*omega[i1,j2]
+            d2L.da02 <- d2L.da02 + z[i]*z[j]*omega[i2,j2]
+            d2L.db02 <- d2L.db02 + z[i]*z[j]*omega[i1,j1]*U^2
+            d2L.da0dt <- d2L.da0dt +
+                Q238*(z[i]+z[j])*omega[i2,j2]/2 +
+                Q235*z[j]*omega[i1,j2]
+            d2L.db0dt <- d2L.db0dt + U*(
+                Q235*(z[i]+z[j])*omega[i1,j1]/2 +
+                Q238*z[i]*omega[i1,j2] )
+            d2L.da0db0 <- d2L.da0db0 +
+                U * z[i]*z[j]*omega[i1,j2]
             d2L.dzdt <- d2L.dzdt +
                 Q235*(U*b0*omega[i1,j1] + a0*omega[i2,j1] + omega[i3,j1]) +
                 Q238*(U*b0*omega[i1,j2] + a0*omega[i2,j2] + omega[i3,j2])
             d2L.dzda0 <- d2L.dzda0 +
-                z[j]*(a0*omega[i2,j2] + U*b0*omega[i1,j2] * omega[i3,j2])
+                z[j]*(a0*omega[i2,j2] + U*b0*omega[i1,j2] + omega[i3,j2])
             d2L.dzdb0 <- d2L.dzdb0 +
                 U*z[j]*(a0*omega[i2,j1] + U*b0*omega[i1,j1] + omega[i3,j1])
             d2L.dzdz <-  (omega[i1,j1] + omega[i1,j3] + omega[i3,j1])*(U*b0)^2 +
                 omega[i2,j2]*a0^2 + omega[i3,j3] + a0*(omega[i2,j3]+omega[i3,j2]) +
-                a0*U*b0*(omega[i1,j2]+omega[i2,j1])
+                                    a0*U*b0*(omega[i1,j2]+omega[i2,j1])
             out[i,j] <- d2L.dzdz # dij
             out[j,i] <- d2L.dzdz # dij
         }
         d2L.dz2 <- omega[i1,i1]*(U*b0)^2 + omega[i2,i2]*a0^2 + omega[i3,i3] +
-                   2*(a0*U*b0*omega[i1,i2] + U*b0*omega[i1,i3] * a0*omega[i2,i3])
+                   2*(a0*U*b0*omega[i1,i2] + U*b0*omega[i1,i3] + a0*omega[i3,i3])
         out[i,i] <- d2L.dz2 # dii
         out[i,ns+1] <- d2L.dzdt # ni1
         out[i,ns+2] <- d2L.dzda0 # ni2
-        out[i,ns+3] <- d2L.dzdb0 # ni2
+        out[i,ns+3] <- d2L.dzdb0 # ni3
         out[ns+1,i] <- d2L.dzdt # ni1
         out[ns+2,i] <- d2L.dzda0 # ni2
-        out[ns+3,i] <- d2L.dzdb0 # ni2
+        out[ns+3,i] <- d2L.dzdb0 # ni3
     }
+    out[ns+1,ns+1] <- d2L.dt2 # m11
+    out[ns+2,ns+2] <- d2L.da02 # m22
+    out[ns+3,ns+3] <- d2L.db02 # m33
+    out[ns+1,ns+2] <- d2L.da0dt # m12
+    out[ns+1,ns+3] <- d2L.db0dt # m13
+    out[ns+2,ns+1] <- d2L.da0dt # m12
+    out[ns+3,ns+1] <- d2L.db0dt # m13
+    out[ns+2,ns+3] <- d2L.da0db0 # m23
+    out[ns+3,ns+2] <- d2L.da0db0 # m23
     out
 }
 
@@ -203,28 +194,32 @@ LL.lud.UPb <- function(ta0b0,x,exterr=FALSE){
     a0 <- ta0b0[2]
     b0 <- ta0b0[3]
     d <- data2ludwig(x,a0=a0,b0=b0,tt=tt,exterr=exterr)
-    if (exterr) out <- S.lud.helper(d)/2
-    else out <- sum(d$S)/2
-    out
-}
-S.lud.helper <- function(d){
     phi <- d$phi
     R <- d$R
     r <- d$r
-    omega <- d$omega
     SS <- 0
     ns <- length(d$R)
-    for (i in 1:ns){
-        i1 <- i
-        i2 <- i + ns
-        i3 <- i + 2*ns
-        for (j in 1:ns){
-            j1 <- j
-            j2 <- j + ns
-            j3 <- j + 2*ns
-            SS <- SS + R[i]*R[j]*omega[i1,j1] + r[i]*r[j]*omega[i2,j2] *
-                  phi[i]*phi[j]*omega[i3,j3] + 2*( R[i]*r[j]*omega[i1,j2] +
-                  R[i]*phi[j]*omega[i1,j3] + r[i]*phi[j]*omega[i2,j3] )
+    if (exterr){
+        omega <- d$omega
+        for (i in 1:ns){
+            i1 <- i
+            i2 <- i + ns
+            i3 <- i + 2*ns
+            for (j in 1:ns){
+                j1 <- j
+                j2 <- j + ns
+                j3 <- j + 2*ns
+                SS <- SS + R[i]*R[j]*omega[i1,j1] + r[i]*r[j]*omega[i2,j2] +
+                    phi[i]*phi[j]*omega[i3,j3] + 2*( R[i]*r[j]*omega[i1,j2] +
+                    R[i]*phi[j]*omega[i1,j3] + r[i]*phi[j]*omega[i2,j3] )
+            }
+        }
+    } else {
+        for (i in 1:ns){
+            omega <- d$omega[[i]]
+            SS <- SS + omega[1,1]*R[i]^2 + omega[2,2]*r[i]^2 + omega[3,3]*phi[i]^2 +
+                2*( R[i]*r[i]*omega[1,2] + R[i]*phi[i]*omega[1,3] +
+                r[i]*phi[i]*omega[2,3] )
         }
     }
     SS
@@ -250,23 +245,23 @@ data2ludwig_without_decay_err <- function(x,a0,b0,tt){
     R <- rep(0,ns)
     r <- rep(0,ns)
     phi <- rep(0,ns)
+    Z <- rep(0,ns)
     z <- rep(0,ns)
-    S <- rep(0,ns)
     omega <- list()
     E <- matrix(0,3,3)
     for (i in 1:ns){
-        d <- wetherill(x,i=i,exterr=FALSE) # will add external errors based on Ludwig (1998)
-        Zi <- d$x['Pb204U238']
-        R[i] <- d$x['Pb207U235'] - exp(l5[1]*tt) + 1 - U*b0*Zi
-        r[i] <- d$x['Pb206U238'] - exp(l8[1]*tt) + 1 - a0*Zi
+        d <- wetherill(x,i=i,exterr=FALSE)
+        Z[i] <- d$x['Pb204U238']
+        R[i] <- d$x['Pb207U235'] - exp(l5[1]*tt) + 1 - U*b0*Z[i]
+        r[i] <- d$x['Pb206U238'] - exp(l8[1]*tt) + 1 - a0*Z[i]
         E[1,1] <- d$cov['Pb207U235','Pb207U235']
         E[2,2] <- d$cov['Pb206U238','Pb206U238']
         E[3,3] <- d$cov['Pb204U238','Pb204U238']
         E[1,2] <- d$cov['Pb207U235','Pb206U238']
-        E[2,1] <- E[1,2]
         E[1,3] <- d$cov['Pb207U235','Pb204U238']
-        E[3,1] <- E[1,3]
         E[2,3] <- d$cov['Pb206U238','Pb204U238']
+        E[2,1] <- E[1,2]
+        E[3,1] <- E[1,3]
         E[3,2] <- E[2,3]
         O <- solve(E)
         omega[[i]] <- O
@@ -277,11 +272,10 @@ data2ludwig_without_decay_err <- function(x,a0,b0,tt){
             (R[i]*a0 + U*b0*r[i])*O[1,2] +
             R[i]*O[1,3] + r[i]*O[2,3]
         CC <- O[1,1]*R[i]^2 + O[2,2]*r[i]^2 + 2*R[i]*r[i]*O[1,2]
-        z[i] <- Zi + BB/AA
-        phi[i] <- Zi - z[i]
-        S[i] <- AA*phi[i]^2 + 2*BB*phi[i] + CC
+        z[i] <- Z[i] + BB/AA
+        phi[i] <- Z[i] - z[i]
     }
-    out <- list(R=R,r=r,phi=phi,z=z,omega=omega,S=S)
+    out <- list(R=R,r=r,phi=phi,z=z,omega=omega)
 }
 data2ludwig_with_decay_err <- function(x,a0,b0,tt){
     # initialise:
@@ -292,13 +286,15 @@ data2ludwig_with_decay_err <- function(x,a0,b0,tt){
     R <- rep(0,ns)
     r <- rep(0,ns)
     Z <- rep(0,ns)
-    P235 <- tt*exp(l5[1]*tt)
-    P238 <- tt*exp(l8[1]*tt)
     E <- matrix(0,3*ns,3*ns)
-    E[1:ns,1:ns] <- (P235*l5[2])^2 # A 
-    E[(ns+1):(2*ns),(ns+1):(2*ns)] <- (P238*l8[2])^2 # B
+    if (TRUE){ # TRUE if propagating decay errors
+        P235 <- tt*exp(l5[1]*tt)
+        P238 <- tt*exp(l8[1]*tt)
+        E[1:ns,1:ns] <- (P235*l5[2])^2 # A 
+        E[(ns+1):(2*ns),(ns+1):(2*ns)] <- (P238*l8[2])^2 # B
+    }
     for (i in 1:ns){
-        d <- wetherill(x,i=i,exterr=FALSE) # will add external errors based on Ludwig (1998)
+        d <- wetherill(x,i=i,exterr=FALSE)
         Z[i] <- d$x['Pb204U238']
         R[i] <- d$x['Pb207U235'] - exp(l5[1]*tt) + 1 - U*b0*Z[i]
         r[i] <- d$x['Pb206U238'] - exp(l8[1]*tt) + 1 - a0*Z[i]
@@ -308,7 +304,7 @@ data2ludwig_with_decay_err <- function(x,a0,b0,tt){
         E[i,ns+i] <- d$cov['Pb207U235','Pb206U238'] # D
         E[ns+i,i] <- E[i,ns+i]
         E[i,2*ns+i] <- d$cov['Pb207U235','Pb204U238'] # E
-        E[2*ns+i,i] <- E[ns+i,2*ns+i]
+        E[2*ns+i,i] <- E[i,2*ns+i]
         E[ns+i,2*ns+i] <- d$cov['Pb206U238','Pb204U238'] # F
         E[2*ns+i,ns+i] <- E[ns+i,2*ns+i]
     }
@@ -324,12 +320,14 @@ data2ludwig_with_decay_err <- function(x,a0,b0,tt){
             j1 <- j
             j2 <- j + ns
             j3 <- j + 2*ns
-            W[i] <- W[i] - R[j] * (U*b0*omega[i1,j1] + a0*omega[i2,j1] + omega[i3,j1]) +
+            W[i] <- W[i] - R[j] * (U*b0*omega[i1,j1] + a0*omega[i2,j1] + omega[i3,j1]) -
                            r[j] * (U*b0*omega[i1,j2] + a0*omega[i2,j2] + omega[i3,j2])
-            V[i,j] <- U*b0*omega[i1,j3] + a0*omega[i2,j3] + omega[i3,j3]
+#            V[i,j] <- U*b0*omega[i1,j3] + a0*omega[i2,j3] + omega[i3,j3]
+            V[i,j] <- omega[i1,j1]*(U*b0)^2 + omega[i2,j2]*a0^2 + omega[i3,j3] +
+                      2*(U*a0*b0*omega[i1,j2] + U*b0*omega[i1,j3] + a0*omega[i2,j3])
         }
     }
     phi <- solve(V,W)
     z <- Z - phi
-    list(R=R,r=r,z=z,phi=phi,omega=omega)
+    out <- list(R=R,r=r,phi=phi,z=z,omega=omega)
 }
