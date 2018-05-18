8c16a05e295cc7adde952929c8bc099f8cc874e2
pvermees <p.vermeesch@ucl.ac.uk>
2018-05-18 17:06:45 +0100

    further fixes to roundit function

--- R/toolbox.R
+++ R/toolbox.R
@@ -24,9 +24,9 @@ roundit <- function(age,err,sigdig=2){
         nd <- ceiling(log10(abs(dat)))-ceiling(log10(min.err))+sigdig
         rounded <- signif(dat,nd)
         decimals <- trunc(log10(min.err))-sigdig
-        if (decimals<0) nsmall <- abs(decimals)
+        if (decimals<1) nsmall <- abs(decimals)
         else nsmall <- 0
-        out <- format(rounded,digits=nsmall-1,nsmall=nsmall,trim=TRUE)
+        out <- format(rounded,digits=max(0,nsmall-1),nsmall=nsmall,trim=TRUE)
     }
     out
 }
