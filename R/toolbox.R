77c1575728bd667afc6f61df99b2aad60180a5be
pvermees <pieter.vermeesch@gmail.com>
2018-09-10 21:29:16 +0100

    Model-2 discordia only uses lm(...), not York(...)

--- R/toolbox.R
+++ R/toolbox.R
@@ -229,7 +229,7 @@ validLevels <- function(levels){
 }
 
 colourbar <- function(z=c(0,1),col=c("#00FF0080","#FF000080"),
-                      strip.width=0.02,clabel=""){
+                      strip.width=0.02,clabel="",cex=1){
     if (!validLevels(z)) return()
     ucoord <- graphics::par()$usr
     plotwidth <- (ucoord[2]-ucoord[1])
@@ -250,7 +250,7 @@ colourbar <- function(z=c(0,1),col=c("#00FF0080","#FF000080"),
     graphics::par(new=T)
     graphics::plot(rep(xe,length(z)),z,type='n',axes=F,xlab=NA,ylab=NA)
     graphics::axis(side=4)
-    graphics::mtext(text=clabel,side=3,adj=1)
+    graphics::mtext(text=clabel,side=3,adj=1,cex=cex)
 }
 
 plot_points <- function(x,y,bg='white',show.numbers=FALSE,...){
