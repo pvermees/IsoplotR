d20a9f5ebab6310d5bb33b8614c62526d636ce56
pvermees <p.vermeesch@ucl.ac.uk>
2019-01-24 14:24:51 +0000

    disequilibrium correction seems to work for ludwig regression!

--- R/UPb.R
+++ R/UPb.R
@@ -699,14 +699,14 @@ dd1dl5 <- function(tt,D0){
 }
 dd2dl8 <- function(tt,A0,B0,C0){
     l8 <- settings('lambda','U238')[1]
-    d1(tt,A0,B0,C0)*(tt+1/l8)
+    d2(tt,A0,B0,C0)*(tt+1/l8)
 }
-dd1dl5 <- function(tt,D0){
+dd1dt <- function(tt,D0){
     l5 <- settings('lambda','U235')[1]
     l1 <- settings('lambda','Pa231')[1]*1000
     l5*d1(tt,D0) + D0*l5*exp((l5-l1)*tt)
 }
-dd2dl8 <- function(tt,A0,B0,C0){
+dd2dt <- function(tt,A0,B0,C0){
     l6 <- settings('lambda','Ra226')[1]*1000
     l0 <- settings('lambda','Th230')[1]*1000
     l4 <- settings('lambda','U234')[1]*1000
