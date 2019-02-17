8b838c7d2a2fd6b86ca183fd395763427b118940
pvermees <p.vermeesch@ucl.ac.uk>
2019-02-17 11:25:49 +0000

    Shortened initial Pb ratio calculation for U-Pb data

--- R/initial.R
+++ R/initial.R
@@ -7,31 +7,24 @@ get.initial.ratio.UPb <- function(x){
     tt <- lud$par[1]
     if (x$format<4){
         rr <- age_to_terawasserburg_ratios(tt,d=x$d)$x
-        tl <- lud$par['t[l]']
-        y0 <- lud$par['76i']
-        slope <- (rr['Pb207Pb206']-y0)/rr['U238Pb206']
-        yp <- y0 + slope*get.U238Pb206.ratios(x)[,1]
-        dy <- get.Pb207Pb206.ratios(x)[,1] - yp
-        out <- y0 + dy
+        slope <- (rr['Pb207Pb206']-lud$par['76i'])/rr['U238Pb206']
+        out <- get.Pb207Pb206.ratios(x)[,1] -
+               slope*get.U238Pb206.ratios(x)[,1]
     } else {
         out <- matrix(0,length(x),2)
         # first determine 6/4-intercepts:
-        y0 <- 1/lud$par['64i'] # 4/6-intercept
+        y0 <- 1/lud$par['64i']     # 4/6-intercept
         x0 <- age_to_U238Pb206_ratio(tt,d=x$d)[1]   # 8/6-intercept
         X <- get.U238Pb206.ratios(x)[,1]            # 8/6 measurement
-        yp <- y0 - X*y0/x0     # predicted 6/4-value
         Y <- X*get.Pb204U238.ratios(x)[,1]          # 4/6 measurement
-        dy <- Y - yp           # 6/4 misfit
-        out[,1] <- 1/(y0 + dy) # initial 6/4-ratio
+        out[,1] <- 1/(Y + X*y0/x0) # initial 6/4-ratio
         # then determine 7/4-intercepts:
         y0 <- 1/lud$par['74i'] # 4/7-intercept
         x0 <- 1/age_to_Pb207U235_ratio(tt,d=x$d)[1] # 5/7-intercept
         X <- 1/get.Pb207U235.ratios(x)[,1]          # 5/7 measurement
-        yp <- y0 - X*y0/x0     # predicted 4/7-value
         U <- settings('iratio','U238U235')[1]
         Y <- X*U*get.Pb204U238.ratios(x)[,1]        # 4/7 measurement
-        dy <- Y - yp           # 7/4 misfit
-        out[,2] <- 1/(y0 + dy) # initial 7/4-ratio
+        out[,2] <- 1/(Y + X*y0/x0) # initial 7/4-ratio
     }
     out
 }
