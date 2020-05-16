a770a8849a4b8497d2832e164ddacf41a455a956
Pieter Vermeesch <p.vermeesch@ucl.ac.uk>
2020-05-16 21:31:37 +0100

    Added xlab and ylab arguments to scatterplot function.

--- R/errorellipse.R
+++ R/errorellipse.R
@@ -43,8 +43,6 @@ ellipse <- function(x,y,covmat,alpha=0.05,n=50){
 #' a third variable as a colour scale.
 #'
 #' @param xy matrix with columns \code{X, sX, Y, sY(, rXY)}
-#' @param xlim (optional) two-element vector with the x-axis limits
-#' @param ylim (optional) two-element vector with the y-axis limits
 #' @param alpha the probability cutoff for the error ellipses
 #' @param show.numbers logical flag (\code{TRUE} to show grain
 #'     numbers)
@@ -97,6 +95,10 @@ ellipse <- function(x,y,covmat,alpha=0.05,n=50){
 #' @param bg background colour for the plot symbols (only used if
 #'     \code{show.ellipses=0}).
 #' @param cex plot symbol magnification.
+#' @param xlim (optional) two-element vector with the x-axis limits
+#' @param ylim (optional) two-element vector with the y-axis limits
+#' @param xlab (optional) x-axis label (only used when \code{add=FALSE})
+#' @param ylab (optional) y-axis label (only used when \code{add=FALSE})
 #' @param ... optional arguments to format the points and text.
 #' 
 #' @examples
@@ -109,26 +111,31 @@ ellipse <- function(x,y,covmat,alpha=0.05,n=50){
 #' dat <- cbind(X,sX,Y,sY)
 #' scatterplot(dat,fit=york(dat),show.ellipses=2)
 #' @export
-scatterplot <- function(xy,xlim=NA,ylim=NA,alpha=0.05,
-                        show.numbers=FALSE,show.ellipses=1,levels=NA,
-                        clabel="",ellipse.col=c("#00FF0080","#FF000080"),
+scatterplot <- function(xy,alpha=0.05,show.numbers=FALSE,
+                        show.ellipses=1,levels=NA,clabel="",
+                        ellipse.col=c("#00FF0080","#FF000080"),
                         fit='none',add=FALSE,empty=FALSE,
                         ci.col='gray80',line.col='black',lwd=1,
                         hide=NULL,omit=NULL,omit.col=NA,
-                        addcolourbar=TRUE,bg,cex,...){
+                        addcolourbar=TRUE,bg,cex,xlim,ylim,xlab,ylab,...){
     ns <- nrow(xy)
     if (ncol(xy)==4) xy <- cbind(xy,rep(0,ns))
     sn <- 1:ns
     plotit <- sn%ni%hide
     calcit <- sn%ni%c(hide,omit)
     colnames(xy) <- c('X','sX','Y','sY','rXY')
-    if (any(is.na(xlim))) xlim <- get.limits(xy[plotit,'X'],xy[plotit,'sX'])
-    if (any(is.na(ylim))) ylim <- get.limits(xy[plotit,'Y'],xy[plotit,'sY'])
-    if (!add) graphics::plot(xlim,ylim,type='n',xlab='',ylab='',bty='n')
-    if (!add & empty) return()
-    if (!identical(fit,'none'))
+    if (missing(xlim)) xlim <- get.limits(xy[plotit,'X'],xy[plotit,'sX'])
+    if (missing(ylim)) ylim <- get.limits(xy[plotit,'Y'],xy[plotit,'sY'])
+    if (!add){
+        if (missing(xlab)) xlab <- ''
+        if (missing(ylab)) ylab <- ''
+        graphics::plot(xlim,ylim,type='n',xlab=xlab,ylab=ylab,bty='n')
+        if (empty) return()
+    }
+    if (!identical(fit,'none')){
         plot_isochron_line(fit,x=seq(xlim[1],xlim[2],length.out=100),
                            ci.col=ci.col,col=line.col,lwd=lwd)
+    }
     graphics::box()
     nolevels <- all(is.na(levels))
     if (show.ellipses==2 && nolevels){
