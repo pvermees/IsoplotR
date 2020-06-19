22aceb57d3c940c1d2b4772d8cf0a51052428a1a
Pieter Vermeesch <p.vermeesch@ucl.ac.uk>
2020-06-19 22:44:13 +0100

    Allows discfilter option to be specified as letters or numbers

--- R/discfilter.R
+++ R/discfilter.R
@@ -3,11 +3,11 @@ discfilter <- function(option='c',before=TRUE,cutoff){
     out$option <- option
     out$before <- before
     if (missing(cutoff)){
-        if (option=='t') cutoff <- c(-5,50)
-        else if (option=='r') cutoff <- c(-5,15)
-        else if (option=='sk') cutoff <- c(-1e-5,1e-4)
-        else if (option=='a') cutoff <- c(-0.01,0.05)
-        else if (option=='c') cutoff <- c(-0.01,0.05)
+        if (option%in%c(0,'t')) cutoff <- c(-5,50)
+        else if (option%in%c(1,'r')) cutoff <- c(-5,15)
+        else if (option%in%c(2,'sk')) cutoff <- c(-0.001,0.01)
+        else if (option%in%c(3,'a')) cutoff <- c(-1,5)
+        else if (option%in%c(4,'c')) cutoff <- c(-1,5)
         else stop('Invalid discordance filter option.')
     }
     out$cutoff <- cutoff
@@ -22,30 +22,30 @@ filter.UPb.ages <- function(x,type=5,cutoff.76=1100,exterr=FALSE,
     } else {
         conc <- (type==5) | (cutoff.disc$option=='c')
         tt <- UPb.age(x,exterr=exterr,conc=conc,common.Pb=common.Pb)
-        if (cutoff.disc$option=='t'){
+        if (cutoff.disc$option%in%c(0,'t')){
             dif <- tt[,'t.76']-tt[,'t.68']
-        } else if (cutoff.disc$option=='r'){
-            dif <- 1 - tt[,'t.68']/tt[,'t.76']
-        } else if (cutoff.disc$option=='sk'){
+        } else if (cutoff.disc$option%in%c(1,'r')){
+            dif <- (1-tt[,'t.68']/tt[,'t.76'])*100
+        } else if (cutoff.disc$option%in%c(2,'sk')){
             U8Pb6.raw <- get.U238Pb206.ratios(x)[,'U238Pb206']
             U8Pb6.corr <- common.Pb.stacey.kramers(x)[,'U238Pb206']
-            dif <- 1 - U8Pb6.raw/U8Pb6.corr
-        } else if (cutoff.disc$option=='a'){
+            dif <- (1-U8Pb6.raw/U8Pb6.corr)*100
+        } else if (cutoff.disc$option%in%c(3,'a')){
             X1 <- age_to_U238Pb206_ratio(tt[,'t.76'])[,1]
             Y1 <- age_to_Pb207Pb206_ratio(tt[,'t.68'])[,1]
             U8Pb6 <- get.U238Pb206.ratios(x)[,'U238Pb206']
             Pb76 <- get.Pb207Pb206.ratios(x)[,'Pb207Pb206']
             DX <- log(U8Pb6) - log(X1)
             DY <- log(Pb76) - log(Y1)
-            dif <- DX*sin(atan(DY/DX))
-        } else if (cutoff.disc$option=='c'){
+            dif <- 100*DX*sin(atan(DY/DX))
+        } else if (cutoff.disc$option%in%c(4,'c')){
             x1 <- age_to_U238Pb206_ratio(tt[,'t.conc'])[,1]
             U8Pb6 <- get.U238Pb206.ratios(x)[,'U238Pb206']
             dx <- log(x1) - log(U8Pb6)
             x2 <- age_to_Pb207Pb206_ratio(tt[,'t.conc'])[,1]
             Pb76 <- get.Pb207Pb206.ratios(x)[,'Pb207Pb206']
             dy <- log(x2) - log(Pb76)
-            dif <- sqrt(dx^2+dy^2)
+            dif <- 100*sqrt(dx^2+dy^2)
         } else {
             stop('Invalid discordance filter option.')
         }
