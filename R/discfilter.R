c9a2bb1bbb88ca79ed4b224938a7a15cc514fed3
Pieter Vermeesch <p.vermeesch@ucl.ac.uk>
2020-06-20 09:41:19 +0100

    Implemented discordance filter before/after common Pb correction.

--- R/discfilter.R
+++ R/discfilter.R
@@ -21,29 +21,35 @@ filter.UPb.ages <- function(x,type=5,cutoff.76=1100,exterr=FALSE,
         is.concordant <- rep(TRUE,length(x))
     } else {
         conc <- (type==5) | (cutoff.disc$option=='c')
-        tt <- UPb.age(x,exterr=exterr,conc=conc,common.Pb=common.Pb)
+        if (cutoff.disc$before | common.Pb==0){
+            X <- x
+            tt <- UPb.age(X,exterr=exterr,conc=conc)
+        } else {
+            X <- Pb0corr(x,option=common.Pb)
+            tt <- UPb.age(X,exterr=exterr,conc=conc,common.Pb=common.Pb)
+        }
         if (cutoff.disc$option%in%c(0,'t')){
             dif <- tt[,'t.76']-tt[,'t.68']
         } else if (cutoff.disc$option%in%c(1,'r')){
             dif <- (1-tt[,'t.68']/tt[,'t.76'])*100
         } else if (cutoff.disc$option%in%c(2,'sk')){
             U8Pb6.raw <- get.U238Pb206.ratios(x)[,'U238Pb206']
-            U8Pb6.corr <- common.Pb.stacey.kramers(x)[,'U238Pb206']
+            U8Pb6.corr <- get.U238Pb206.ratios(X)[,'U238Pb206']
             dif <- (1-U8Pb6.raw/U8Pb6.corr)*100
         } else if (cutoff.disc$option%in%c(3,'a')){
             X1 <- age_to_U238Pb206_ratio(tt[,'t.76'])[,1]
             Y1 <- age_to_Pb207Pb206_ratio(tt[,'t.68'])[,1]
-            U8Pb6 <- get.U238Pb206.ratios(x)[,'U238Pb206']
-            Pb76 <- get.Pb207Pb206.ratios(x)[,'Pb207Pb206']
+            U8Pb6 <- get.U238Pb206.ratios(X)[,'U238Pb206']
+            Pb76 <- get.Pb207Pb206.ratios(X)[,'Pb207Pb206']
             DX <- log(U8Pb6) - log(X1)
             DY <- log(Pb76) - log(Y1)
             dif <- 100*DX*sin(atan(DY/DX))
         } else if (cutoff.disc$option%in%c(4,'c')){
             x1 <- age_to_U238Pb206_ratio(tt[,'t.conc'])[,1]
-            U8Pb6 <- get.U238Pb206.ratios(x)[,'U238Pb206']
+            U8Pb6 <- get.U238Pb206.ratios(X)[,'U238Pb206']
             dx <- log(x1) - log(U8Pb6)
             x2 <- age_to_Pb207Pb206_ratio(tt[,'t.conc'])[,1]
-            Pb76 <- get.Pb207Pb206.ratios(x)[,'Pb207Pb206']
+            Pb76 <- get.Pb207Pb206.ratios(X)[,'Pb207Pb206']
             dy <- log(x2) - log(Pb76)
             dif <- 100*sqrt(dx^2+dy^2)
         } else {
@@ -58,6 +64,9 @@ filter.UPb.ages <- function(x,type=5,cutoff.76=1100,exterr=FALSE,
                     '(if you have already applied a common-Pb correction), ',
                     'apply the discordance filter before the ',
                     'common-Pb correction.'))
+    }
+    if (!cutoff.disc$before & common.Pb!=0){
+        tt <- UPb.age(X,exterr=exterr,conc=conc,common.Pb=common.Pb)
     }
     out <- matrix(NA,length(x),2)
     if (type==1){
