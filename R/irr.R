2d1f57c44d82c1bb49768e98a7ee5128d4a8b8ea
pvermees <p.vermeesch@ucl.ac.uk>
2022-05-28 21:46:41 +0100

    Invertible isochrons work except for overdispersion

--- R/irr.R
+++ R/irr.R
@@ -8,21 +8,29 @@ irr.default <- function(x,alpha=0.05,...){
     g <- log((v-a)/b)
     init <- c(a,b,g)
     XYZ <- york2clr(x)
-    fit <- stats::optim(init,LL.irr.generic,XYZ=XYZ)
+    out <- stats::optim(init,LL.irr.generic,XYZ=XYZ)
+    out
 }
 irr.RbSr <- function(x,alpha=0.05,model=1,inverse=FALSE,...){
-    yfit <- isochron(x,plot=FALSE)
-    y <- yfit$xyz
-    ap <- log(yfit$a[1])
+    yfit <- isochron(x,inverse=inverse,plot=FALSE)
     bp <- log(yfit$age[1])
-    ydat <- data2york(x)
+    ydat <- yfit$xyz
     v <- log(get.york.xy(ydat,a=yfit$a[1],b=yfit$b[1])[,2])
-    g <- log((v-ap)/yfit$b[1])
-    init <- c(ap,bp,g)
+    lam <- lambda('Rb87')
+    if (inverse){
+        ap <- -log(yfit$a[1])
+        a <- -ap
+        b <- -exp(-ap)*(exp(lam[1]*exp(bp))-1)
+    } else {
+        ap <- log(yfit$a[1])
+        a <- ap
+        b <- exp(lam[1]*exp(bp))-1
+    }
+    g <- log((v-a)/b)
+    init <- c(ap,bp,g,0)
     XYZ <- york2clr(ydat)
-    fit <- stats::optim(init,LL.irr.a,XYZ=XYZ,
-                        lam=lambda('Rb87'),inverse=inverse)
-    fit
+    out <- stats::optim(init,LL.irr.a,XYZ=XYZ,lam=lam,inverse=inverse)
+    out
 }
 LL.irr.generic <- function(abg,XYZ){
     ns <- length(XYZ)
@@ -38,17 +46,17 @@ LL.irr.generic <- function(abg,XYZ){
         D <- XYZ[[i]]$XYZ - xyz
         out <- out + t(D) %*% XYZ[[i]]$O %*% D
     }
-    -out/2
+    out/2
 }
 LL.irr.a <- function(abgw,XYZ,lam,inverse=FALSE){
     wgba <- rev(abgw)
     abg <- rev(wgba[-1])
-    LL.irr.a.w(abg,XYZ=XYZ,w=wgba[1],lam=lam,inverse=inverse)
+    LL.irr.a.w(abg,XYZ=XYZ,w=-Inf,lam=lam,inverse=inverse)
 }
 LL.irr.a.w <- function(abg,XYZ,lam,w=-Inf,inverse=FALSE){
     if (inverse){
         a <- -abg[1]
-        b <- -exp(abg[1])*(exp(lam[1]*exp(abg[2]))-1)
+        b <- -exp(-abg[1])*(exp(lam[1]*exp(abg[2]))-1)
     } else {
         a <- abg[1]
         b <- exp(lam[1]*exp(abg[2]))-1
@@ -60,17 +68,14 @@ LL.irr.a.w <- function(abg,XYZ,lam,w=-Inf,inverse=FALSE){
     out <- 0
     if (is.finite(w)){
         Ei <- matrix(0,4,4)
-        Ei[4,4] <- exp(w)
-        du.da <- (1-exp(a))/(v-exp(a))
-        da.dap <- ifelse(inverse,-1,1)
+        Ei[4,4] <- exp(w)^2
         Ji <- matrix(0,3,4)
         Ji[1:3,1:3] <- diag(3)
+        Ji[1:3,4] <- -c(1,1,-2)/3
     }
     for (i in seq_along(XYZ)){
         if (is.finite(w)){
             Ei[1:3,1:3] <- XYZ[[i]]$E
-            dxyz.da <- J %*% c(du.da[i],1)
-            Ji[,4] <- dxyz.da*da.dap
             Eip <- Ji %*% Ei %*% t(Ji)
             O <- MASS::ginv(Eip)
         } else {
@@ -80,7 +85,7 @@ LL.irr.a.w <- function(abg,XYZ,lam,w=-Inf,inverse=FALSE){
         D <- XYZ[[i]]$XYZ - xyz
         out <- out + t(D) %*% O %*% D
     }
-    -out/2
+    out/2
 }
 LL.irr.b <- function(abgw,XYZ){
 }
@@ -109,6 +114,22 @@ york2clr <- function(x){
     out
 }
 
-alr2clr <- function(u,v){
-
+clr2york <- function(XYZ){
+    J <- matrix(0,2,3)
+    ns <- length(XYZ)
+    out <- matrix(0,ns,5)
+    colnames(out) <- c('X','sX','Y','sY','rXY')
+    for (i in 1:ns){
+        out[i,'X'] <- exp(XYZ[[i]]$XYZ[1]-XYZ[[i]]$XYZ[3])
+        out[i,'Y'] <- exp(XYZ[[i]]$XYZ[2]-XYZ[[i]]$XYZ[3])
+        J[1,1] <- out[i,'X']
+        J[1,3] <- -out[i,'X']
+        J[2,2] <- out[i,'Y']
+        J[2,3] <- -out[i,'Y']
+        covmat <- J %*% XYZ[[i]]$E %*% t(J)
+        out[i,'sX'] <- sqrt(covmat[1,1])
+        out[i,'sY'] <- sqrt(covmat[2,2])
+        out[i,'rXY'] <- covmat[1,2]
+    }
+    out
 }
