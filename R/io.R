e70ba463dca9eb9ba8bdaf7e03adc280d2e82c8f
pvermees <p.vermeesch@ucl.ac.uk>
2022-09-14 15:03:38 +0100

    Different zero handling for UThHe data

--- R/io.R
+++ R/io.R
@@ -717,7 +717,7 @@ as.UThHe <- function(x,ierr=1){
     nr <- nrow(x)
     if (is.numeric(x)) X <- x
     else X <- shiny2matrix(x,2,nr,nc)
-    X[X<=0] <- NA
+    X[X<0] <- NA
     X <- errconvert(X,gc='U-Th-He',ierr=ierr)
     if (nc>5) cnames <- c('He','errHe','U','errU','Th','errTh')
     if (nc>7) cnames <- c(cnames,'Sm','errSm')
