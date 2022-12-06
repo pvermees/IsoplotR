aaa11958fa416bda1b9092a709429aec3b95d9f2
pvermees <p.vermeesch@ucl.ac.uk>
2022-12-06 22:53:40 +0000

    Updated evolution legend

--- R/evolution.R
+++ R/evolution.R
@@ -338,7 +338,7 @@ Th02vsU8Th2 <- function(x,isochron=FALSE,model=1,Th0i=0,xlim=NULL,
         graphics::title(xlab=xlab,ylab=ylab)
         if (Th0i==0){
             tit <- expression(paste("[isochrons assume ("^"230","Th/"^
-                                    "232","Th)"[0]*" = 0]"))
+                                    "232","Th)"[i]*" = 0]"))
             mymtext(tit,line=0,...)
         }
         if (Th0i==2){ # add equiline
@@ -362,7 +362,7 @@ evolution.title <- function(fit,sigdig=2,oerr=3,...){
         content[[3]] <- mswdtit(mswd=fit$mswd,p=fit$p.value,sigdig=sigdig)
     }
     if (fit$model==3) {
-        prefix <- quote('('^232*'Th/'^238*'U)-'*dispersion~'=')
+        prefix <- quote('('^234*'U/'^238*'U)'[a]*'-dispersion = ')
         content[[3]] <- disptit(w=fit$disp[1],sw=fit$disp[-1],sigdig=sigdig,
                                     oerr=oerr,units='',prefix=prefix)
     }
