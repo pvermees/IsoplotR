a30871442b7bda6865e06364c67f24c4f289d3e0
pvermees <p.vermeesch@ucl.ac.uk>
2025-04-06 10:43:27 +0100

    Added NA checker to validLevels

--- R/toolbox.R
+++ R/toolbox.R
@@ -274,7 +274,11 @@ levels2colours <- function(levels=c(0,1),col=c('yellow','red')){
 }
 
 validLevels <- function(levels){
-    length(levels)>0 && all(is.numeric(levels)) && diff(range(levels,na.rm=TRUE))!=0
+    notempty <- length(levels)>0
+    allnumeric <- all(is.numeric(levels))
+    notallna <- !all(is.na(levels))
+    hasrange <- diff(range(levels,na.rm=TRUE))
+    notempty && allnumeric && notallna && hasrange
 }
 
 colourbar <- function(z=c(0,1),fill=c("#00FF0080","#FF000080"),
