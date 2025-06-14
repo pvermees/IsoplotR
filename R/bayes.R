d68a7f76ead2a1c71b00883b63ae3ed025ff1d41
Pieter <p.vermeesch@ucl.ac.uk>
2025-06-14 23:07:32 +0100

    Changed LLlist to Llist in bayesplot

--- R/bayes.R
+++ R/bayes.R
@@ -76,14 +76,14 @@ bayeslud <- function(fit,x,anchor=0,type='joint',model=1,
     if (plot) bayesplot(out,fit,add=add)
     out
 }
-bayesplot <- function(LLlist,fit,add=FALSE){
-        nbpar <- length(LLlist)
+bayesplot <- function(Llist,fit,add=FALSE){
+    nbpar <- length(Llist)
         if (!add) op <- graphics::par(mfrow=c(1,nbpar))
-        for (bpar in names(LLlist)){
-            plot(LLlist[[bpar]],type='b',xlab=bpar)
+        for (bpar in names(Llist)){
+            plot(Llist[[bpar]],type='b',xlab=bpar)
             if (bpar=='t') xx <- exp(fit$par[bpar])
             else xx <- fit$par[bpar]
-            graphics::lines(rep(xx,2),range(LLlist[[bpar]][,2]))
+            graphics::lines(rep(xx,2),range(Llist[[bpar]][,2]))
         }
         if (!add) graphics::par(op)
 }
