5c630ee899045f1332e816d019468849e72efd50
Pieter Vermeesch <p.vermeesch@ucl.ac.uk>
2026-03-21 15:52:28 +0000

    Added anchored regression option to LRisochron function

--- R/LRisochron.R
+++ R/LRisochron.R
@@ -2,33 +2,66 @@ LRisochron <- function(x,...){ UseMethod("LRisochron",x) }
 LRisochron.default <- function(x,left=TRUE,...){
     stop("No default method implemented yet.")
 }
-LRisochron.PbPb <- function(x,...){
+LRisochron.PbPb <- function(x,anchor=0,...){
     yd <- data2york(x,inverse=TRUE)
-    lower <- c(-Inf,0,0,min(yd[,'Y']))
-    upper <- c(Inf,1,Inf,max(yd[,'Y']))
-    # init
-    y0i <- yd[which.min(yd[,'X']),'Y']
     gi <- 0
     propi <- 0.5
+    y0i <- yd[which.min(yd[,'X']),'Y']
     sigi <- sd((y0i-yd[,'Y'])/yd[,'X'])
-    init <- c(gi,propi,sigi,y0i)
-    # fit
+    x1 <- y1 <- y0 <- NULL
+    if (anchor[1]==1 & length(anchor)>1){
+        y0 <- age2ratio(tt=anchor[2],ratio="Pb207Pb206")[1]
+        init <- c(gi,propi,sigi)
+        lower <- c(-Inf,0,0)
+        upper <- c(Inf,1,Inf)
+    } else if (anchor[1]==2){
+        Pb74 <- iratio('Pb207Pb204')[1]
+        Pb64 <- iratio('Pb206Pb204')[1]
+        y1 <- Pb74/Pb64
+        x1 <- 1/Pb64
+        init <- c(propi,sigi,y0i)
+        lower <- c(0,0,min(yd[,'Y']))
+        upper <- c(1,Inf,max(yd[,'Y']))
+    } else {
+        init <- c(gi,propi,sigi,y0i)
+        lower <- c(-Inf,0,0,min(yd[,'Y']))
+        upper <- c(Inf,1,Inf,max(yd[,'Y']))
+    }
     fit <- stats::optim(par=init,fn=get_LRisochron_L,
                         method='L-BFGS-B',
                         lower=lower,upper=upper,
-                        yd=yd,fun=yd2ratios.PbPb,
+                        fun=yd2ratios.PbPb,
+                        yd=yd,y0=y0,x1=x1,y1=y1,
                         hessian=TRUE)
-    covmat <- solve(fit$hessian)
-    a <- fit$par[4]
-    sa <- sqrt(covmat[4,4])
-    b <- -fit$par[1]
-    sb <- sqrt(covmat[1,1])
+    covmat <- inverthess(fit$hessian)
+    np <- length(fit$par)
+    if (is.null(y0)){
+        a <- fit$par[np]
+        sa <- sqrt(covmat[np,np])
+    } else {
+        a <- y0
+        sa <- 0
+        cov.ab <- 0
+    }
+    if (is.null(x1) | is.null(y1)){
+        b <- -fit$par[1]
+        sb <- sqrt(covmat[1,1])
+        if (is.null(y0)) cov.ab <- covmat[1,np]
+    } else {
+        b <- (y1-a)/x1
+        sb <- sa/x1
+        rho <- -1
+        cov.ab <- rho*sa*sb
+    }
     list(a=c('a'=a,'s[a]'=sa),
          b=c('b'=b,'s[b]'=sb),
-         cov.ab=covmat[1,4])
+         cov.ab=cov.ab)
 }
 
 LRisochron.ThU <- function(x,UTh=NULL,...){
+    if (x$format<3){
+        stop("Rightmost isochrons are only available for ThU formats 3 and 4.")
+    }
     yd <- data2york(x,type=1)
     lower <- c(-Inf,0,0)
     upper <- c(Inf,1,Inf)
@@ -48,7 +81,8 @@ LRisochron.ThU <- function(x,UTh=NULL,...){
     fit <- stats::optim(par=init,fn=get_LRisochron_L,
                         method='L-BFGS-B',
                         lower=lower,upper=upper,
-                        yd=yd,y0=y0,fun=yd2ratios.ThU,
+                        fun=yd2ratios.ThU,
+                        yd=yd,y0=y0,
                         hessian=TRUE)
     covmat <- inverthess(fit$hessian)
     b <- fit$par[1]
@@ -96,12 +130,18 @@ yd2ratios.ThU <- function(yd,y0){
     cbind(r,sr)
 }
 
-get_LRisochron_L <- function(pars,yd,y0=NULL,fun=yd2ratios){
-    gam <- pars[1]
+get_LRisochron_L <- function(pars,yd,y0=NULL,x1=NULL,y1=NULL,fun=yd2ratios){
+    if (is.null(y0)){
+        y0 <- pars[4]
+    }
+    if (is.null(x1) || is.null(y1)){
+        gam <- pars[1]
+    } else {
+        gam <- (y0-y1)/x1
+    }
     prop <- pars[2]
     sig <- pars[3]
-    mu <- pars[1]
-    if (is.null(y0)) y0 <- pars[4]
+    mu <- gam
     zs <- fun(yd=yd,y0=y0)
     z <- zs[,1]
     s <- zs[,2]
@@ -118,3 +158,4 @@ get_LRisochron_L <- function(pars,yd,y0=NULL,fun=yd2ratios){
     fu[fu>.Machine$double.xmax] <- .Machine$double.xmax
     sum(-log(fu))
 }
+
